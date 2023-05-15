9cae39b44c0b1c929c900feebf84dde0745afbdb
Abdus Salam <abdus786salam@gmail.com>
2023-05-15 11:28:15 +0530

    api with rate limit update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-const connection = mongoose.connect(process.env.mongoDBurl)
+const connection = mongoose.connect('mongodb+srv://salamee:masai@cluster0.haanewi.mongodb.net/bookstore?retryWrites=true&w=majority')
 
 module.exports={
     connection
