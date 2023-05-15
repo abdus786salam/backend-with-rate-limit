55a1da511fd29494ebfbd2ec7645aec10db77ce6
Abdus Salam <abdus786salam@gmail.com>
2023-05-15 13:17:36 +0530

    updated mongodb url

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-const connection = mongoose.connect('mongodb+srv://salamee:masai@cluster0.haanewi.mongodb.net/bookstore?retryWrites=true&w=majority')
+const connection = mongoose.connect(process.env.mongoDBurl)
 
 module.exports={
     connection
